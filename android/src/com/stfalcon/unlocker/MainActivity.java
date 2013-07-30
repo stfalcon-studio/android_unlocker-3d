ab448b589a286760b84e8d3d997a5b308ed6889f
Aleksandr Zozulja <alexz89ua@gmail.com>
2013-07-30 15:18:19 +0300

    first

---
+++ android/src/com/stfalcon/unlocker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.stfalcon.unlocker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
