846f9692a2a48282ece802fc85f4e09ef89188e1
Aleksandr Zozulja <alexz89ua@gmail.com>
2013-08-02 18:06:36 +0300

    unlock screen

---
+++ android/src/com/stfalcon/unlocker/UnlockScreen.java
@@ -0,0 +1,21 @@
+package com.stfalcon.unlocker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by user on 8/2/13.
+ */
+public class UnlockScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock);
+
+        Button unlock = (Button)findViewById(R.id.button_unlock);
+
+        
+    }
+}
